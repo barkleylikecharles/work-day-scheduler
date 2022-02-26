a3ae1363e60b3b53f855f897de0b78e2764c427d
BarkleyLikeCharles <coley@Nicoles-MBP.lan1>
2022-02-26 11:47:38 -0500

    updated buttons with icon and functionality

--- assets/script.js
+++ assets/script.js
@@ -2,4 +2,21 @@
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
-//Function
+//Function for each hour button to save info written in the text area and display
+$(document).ready(function (){
+    $(".saveBtn").on("click", function () {
+//Get values
+        var text = $(this).siblings("description").val();
+        var time = $(this).parent().attr("id");
+// Save to local storage        
+        localStorage.setItem(time, text);
+    })
+//Function to track the current time using moment
+
+function timeTracker() {
+    var timeNow = moment().hour();
+    
+}
+
+
+})
