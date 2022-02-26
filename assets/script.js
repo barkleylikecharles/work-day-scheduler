06a3a2d44e6b0993b755491fbf09012702611516
BarkleyLikeCharles <coley@Nicoles-MBP.lan1>
2022-02-26 12:27:09 -0500

    tracking and styling of time blocks

--- assets/script.js
+++ assets/script.js
@@ -15,8 +15,40 @@ $(document).ready(function (){
 
 function timeTracker() {
     var timeNow = moment().hour();
-    
-}
+// Split hours into blocks and add past, present and future classes
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
+        if (blockTime < timeNow) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (blockTime === timeNow) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+//Pull all hour data from localStorage
+$("#hour8.description").val(localStorage.getItem("hour8"));
+$("#hour9.description").val(localStorage.getItem("hour9"));
+$("#hour10.description").val(localStorage.getItem("hour10"));
+$("#hour11.description").val(localStorage.getItem("hour11"));
+$("#hour12.description").val(localStorage.getItem("hour12"));
+$("#hour13.description").val(localStorage.getItem("hour13"));
+$("#hour14.description").val(localStorage.getItem("hour14"));
+$("#hour15.description").val(localStorage.getItem("hour15"));
+$("#hour16.description").val(localStorage.getItem("hour16"));
+$("#hour17.description").val(localStorage.getItem("hour17"));
+
+//Call the time tracker function once local storage pulled
+timeTracker();
 
 })
